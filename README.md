8e83eae5f1c77f9f6f9e7047d2efbbc6fc0decfa
Wokzy <yegor6741@gmail.com>
2022-05-08 19:08:23 +0700

    Example containers added

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
  - removes docker ownerless <none> images
 
 ### Additional data
- > pyclient and pyserver building directories are made for example and perhaps aren't working yet,
- due to containers link apsence
- > Logs after each script lunch are saved in docker_cleaner.log
+ - Docker compose file is ready to work, it starts 2 containers with pyserver and pyclient made on socket lib. The localhost network is used. Client sends message each 5 sec during one minute, then shutdowns, server countinues to be upped.
+ - Logs after each script lunch are saved in docker_cleaner.log
--- pyclient_build/client.py
+++ pyclient_build/client.py
@@ -1,10 +1,19 @@
-import socket
+import socket, time
+from datetime import datetime
 
 sock = socket.socket()
-sock.connect(('127.0.0.1', 15250))
-sock.send('hello, world!')
+sock.connect(('localhost', 1234))
 
-data = sock.recv(1024)
+c = datetime.now()
+
+while (datetime.now() - c).total_seconds() < 61:
+	time_passed = int((datetime.now() - c).total_seconds())
+	if time_passed % 5 == 0:
+		sock.send(bytes(f'time passed: {time_passed}\n'.encode('ascii')))
+		print(sock.recv(1024).decode('ascii'))
+		time.sleep(1)
+
+
+sock.shutdown(socket.SHUT_RDWR)
+time.sleep(0.1)
 sock.close()
-
-print(data) # ajobka 1 1 21212 1221 123
